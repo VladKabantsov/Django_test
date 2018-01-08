02ce3ab84677c6413a464e94c3af9a3d3621b13a
KabantsoVlad <kabantsovlad@gmail.com>
2018-01-08 22:37:06 +0200

    finished lesson BOOTSTRAP

--- untitled/settings.py
+++ untitled/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'landing',
+    'mainApp',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+
+
--- untitled/urls.py
+++ untitled/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls import url, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('landing/', include('landing.urls')),
+    url(r'^$', include('mainApp.urls'))
 ]
